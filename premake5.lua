80d5273b00e6ea8f0c06838446ff5bb7b0b0e41d
OleStrohm <strohm99@gmail.com>
2020-04-17 01:42:50 +0100

    Switched from custom make to premake
    Premake creates the necessary make files and allows easy separation
    of code and build repositories, as well as multiple projects defined
    by their root directory - kernel & libc

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+workspace "os"
+	configurations { "Debug" }
+
+targetdir "build/%{prj.name}/bin/%{cfg.buildcfg}"
+objdir "build/%{prj.name}/obj"
+
+filter { "action:gmake*" }
+	buildoptions { "-c -std=gnu99 -ffreestanding -O2 -Wall -Wextra" }
+	linkoptions { "-ffreestanding", "-O2", "-nostdlib" }
+
+filter { "configurations:Debug" }
+	defines { "DEBUG" }
+	symbols "On"
+
+project "kernel"
+	kind "ConsoleApp"
+	language "C"
+	location "kernel/"
+	includedirs { "%{prj.location}/include/" }
+	links { "libc" }
+
+	files { "%{prj.location}/**.o",
+			"%{prj.location}/**.h",
+			"%{prj.location}/**.hpp",
+			"%{prj.location}/**.c",
+			"%{prj.location}/**.cpp",
+			"%{prj.location}/**.s" }
+	
+project "libc"
+	kind "SharedLib"
+	language "C++"
+	location "libc/"
+
+	files { "%{prj.location}/**.o",
+			"%{prj.location}/**.h",
+			"%{prj.location}/**.hpp",
+			"%{prj.location}/**.c",
+			"%{prj.location}/**.cpp",
+			"%{prj.location}/**.s" }
